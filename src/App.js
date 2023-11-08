fac59404a253c6f43f5b1ca925395e50f5ef99be
Mac <mac@Macs-MacBook-Pro.local>
2023-11-08 13:19:38 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Avatar,
+  Card,
+  CardMedia,
+  Container,
+  Divider,
+  Typography,
+} from "@mui/material";
+import bg from "../src/images/bg-pattern-card.svg";
+import avatar from "../src/images/image-victor.jpg";
+import { Box, Stack } from "@mui/system";
 
+const typo = {
+  fontFamily: "unset",
+  fontSize: "18px",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main">
+      <div className="flex">
+        <Container
+          sx={{ background: "white", borderRadius: "20px", width: "400px" }}
+          disableGutters
         >
-          Learn React
-        </a>
-      </header>
+          <Card
+            sx={{
+              position: "relative",
+              overflow: "visible",
+              borderTopLeftRadius: "20px",
+              borderTopRightRadius: "20px",
+            }}
+          >
+            <CardMedia component="img" src={bg} />
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%,50%)",
+              }}
+            >
+              <Avatar
+                alt="Remy Sharp"
+                src={avatar}
+                sx={{ width: "100px", height: "100px" }}
+              />
+            </div>
+          </Card>
+
+          <Box mt={10} mb={3} textAlign="center">
+            <Typography className="heading" sx={{ ...typo }}>
+              Victor Crest{" "}
+              <span style={{ color: "hsl(0, 0%, 59%)", fontWeight: "normal" }}>
+                26
+              </span>
+            </Typography>
+
+            <Typography
+              sx={{
+                ...typo,
+                color: "hsl(227, 10%, 46%)",
+                mb: 4,
+                fontSize: "14px",
+              }}
+            >
+              London
+            </Typography>
+
+            <Divider />
+            <Stack
+              mt={4}
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <Typography sx={{ ...typo }} className="heading">
+                80k{" "}
+              </Typography>
+              <Typography sx={{ ...typo }} className="heading">
+                803k{" "}
+              </Typography>
+              <Typography sx={{ ...typo }} className="heading">
+                1.4k{" "}
+              </Typography>
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <Typography
+                sx={{
+                  ...typo,
+                  color: "hsl(0, 0%, 59%)",
+                  fontSize: "12px",
+                  letterSpacing: "2px",
+                }}
+              >
+                Followers
+              </Typography>
+              <Typography
+                sx={{
+                  ...typo,
+                  color: "hsl(0, 0%, 59%)",
+                  fontSize: "12px",
+                  letterSpacing: "2px",
+                }}
+              >
+                Likes{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  ...typo,
+                  color: "hsl(0, 0%, 59%)",
+                  fontSize: "12px",
+                  letterSpacing: "2px",
+                }}
+              >
+                Photos{" "}
+              </Typography>
+            </Stack>
+          </Box>
+        </Container>
+      </div>
     </div>
   );
 }
